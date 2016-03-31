81d0dbcd7a674df6f55e00fdc98dbee9bb39b12d
Jose Ramirez <jcramireztello@gmail.com>
2016-03-31 12:09:46 -0500

    agregando

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -184,8 +184,10 @@ module.exports.marker_map = function(from, to, map){
         });
 
        plan.setAddress(name, coordinate.lng + ',' + coordinate.lat, function(err, rees) {
-                plan.updateRoutes();
+                //plan.updateRoutes();
                 console.log("actualiza address");
+
+                document.getElementById("locations-form").submit();
           });
         }, 150, true);
 
