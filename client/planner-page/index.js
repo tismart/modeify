00cfe0639794395c05503e30b8b0eb6400ed38bc
Jonathan quiza <jony327@gmail.com>
2016-04-02 20:32:22 -0500

    adding console session plan

--- client/planner-page/index.js
+++ client/planner-page/index.js
@@ -340,6 +340,7 @@ function updateMapOnPlanChange(plan, map) {
         if(!(session_current === null)) {
 
                 var session_plan = session_current.plan;
+                console.log("session_plan ->", session_plan);
                 var itineraries = session_plan.itineraries;
 
                 showMapView.marker_map(
