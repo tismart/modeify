37be424dfa23c8c201c661e19b8c4a8d3f82b3ac
jonathan <Furystrikee123>
2016-04-04 12:04:27 -0500

    change import

--- client/planner-page/index.js
+++ client/planner-page/index.js
@@ -20,7 +20,7 @@ var showWelcomeWizard = require('welcome-flow');
 var showPlannerWalkthrough = require('planner-walkthrough');
 var geocode = require('geocode');
 
-var leaflet_cluster = require('./leaflet_label/leaflet.markercluster-src.js');
+var leaflet_cluster = require('./../map-view/leaflet_label/leaflet.markercluster-src.js');
 
 
 var FROM = config.geocode().start_address;
