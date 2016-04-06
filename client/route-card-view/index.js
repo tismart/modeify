a3bb958aa8f8f65820c7b0769a9e34b7d8036c8a
jonathan <Furystrikee123>
2016-04-06 12:31:47 -0500

    adding styles css3

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -34,6 +34,7 @@ var View = module.exports = view(require('./template.html'), function(view, mode
 
       var rec2 = d3.selectAll(".leaflet-div-icon1");
       rec2.attr('class', 'leaflet-marker-icon leaflet-div-icon2 leaflet-zoom-hide');
+      console.log(Math.random())
 
 
   /*
