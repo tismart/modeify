2bf0780216966276dcee14e3c1176ad4a1712099
jonathan <Furystrikee123>
2016-04-06 12:55:35 -0500

    adding styles css3

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -26,8 +26,9 @@ var View = module.exports = view(require('./template.html'), function(view, mode
       var element = [];
       for (var i=0; i<itineration.length;i++) {
            if (i!=model.index){
-                var r3 = d3.selectAll(".iteration-"+i);
-                r3.transition().duration(500).style("stroke", "#E0E0E0");
+                var r3 = d3.selectAll(".iteration-"+i)
+                 .style("opacity", 1)
+                 .transition().duration(500).style("opacity", 0);
            }
       }
 
